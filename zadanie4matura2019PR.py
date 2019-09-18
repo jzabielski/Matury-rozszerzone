e05e142f8562068da5768a5201948c6ad3bee632
Jelonek17 <45925012+Jelonek17@users.noreply.github.com>
2019-09-18 11:46:08 +0200

    zadanie4matura2019PR
    zadanie4matura2019PR

---
+++ zadanie4matura2019PR.py
@@ -0,0 +1,47 @@
+# zadanie 4 Matura
+# Karolina Dudkowska, wrzesień 2019 r.
+# LO w Sobolewie
+def silnia(n):
+    if n ==0:
+        return 1
+    else:
+        return a * silnia(n-1)
+
+def suma_silni(n):
+    suma = 0
+    for cyfra in n:
+        suma += silnia(int(cyfra))
+        return suma
+
+licznik1 = 0
+
+
+
+
+
+potegi = []
+i = 1
+while i <100_000
+    potegi.append(i)
+    i += 3
+
+print(potegi)
+function_czy_potega(n):
+
+
+
+
+z4_2 = "4.2" + "\n"
+with open("przykład.txt", 'r') as plik:
+    for i in plik:.
+        i = i.strip()
+        if suma_silni(i)==int(i):
+            z4_2 = z4_2 + i + "\n"
+        i = int(i)
+        if i in potegi:
+            licznik1 += 1
+
+
+print(licznik)
+with open ("wynik4.txt", "w") as wynik:
+    wynik.write("4.1" + "\n" + "\n")

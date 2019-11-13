1753808db6179286979e98f873d022c2d265527b
jgoliszewksi <jakub.goliszewksi01@gmail.com>
2019-11-13 13:28:43 +0100

    matura 2016 zad 6

---
+++ matura2016PR/zadanie6.py
@@ -0,0 +1,31 @@
+def szyforwanie(slowo, n):
+    szyfr = ""
+    for i in slowo:
+        nr = ord(i) + n
+        while nr > 90:
+            nr -=26
+        szyfr += chr(nr)
+    return szyfr
+
+
+odp1 = []
+with open('Dane_PR2/dane_6_1.txt','r') as plik:
+    for slowo in plik:
+        slowo = slowo.strip()
+        odp1.append(szyforwanie(slowo,107))
+
+odp3 = []
+with open('Dane_PR2/dane_6_3.txt','r') as plik:
+    for line in plik:
+        zle = 0
+        line = line.strip()
+        slowo1, slowo2 = line.split()
+        if ord(slowo2[0]) > ord(slowo1[0]):
+            k = ord(slowo2[0]) - ord(slowo1[0])
+        else:
+            k = ord(slowo2[0]) - ord(slowo1[0]) + 26
+        if szyforwanie(slowo1,k) != slowo2:
+            odp3.append(slowo1)
+
+
+print(odp3)

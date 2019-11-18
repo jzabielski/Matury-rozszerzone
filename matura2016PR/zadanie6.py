a452ac5cf57c3232915dd92bc8fec668f8413deb
jgoliszewksi <jakub.goliszewksi01@gmail.com>
2019-11-18 11:53:52 +0100

    zadanie6 matura 2016

--- matura2016PR/zadanie6.py
+++ matura2016PR/zadanie6.py
@@ -27,5 +27,9 @@ with open('Dane_PR2/dane_6_3.txt','r') as plik:
         if szyforwanie(slowo1,k) != slowo2:
             odp3.append(slowo1)
 
-
-print(odp3)
+with open('wyniki_6_1.txt','w') as wynik:
+    for s in odp1:
+        wynik.write(s + '\n')
+with open('wyniki_6_2.txt','w') as wynik:
+    for s in odp3:
+        wynik.write(s + '\n')

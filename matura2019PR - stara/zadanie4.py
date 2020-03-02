63f397285141360e5fad729a4563b1454849a49e
JGoliszewski <45105582+jgoliszewski@users.noreply.github.com>
2020-03-02 13:00:57 +0100

    matura 2019 stara
    koniec

--- matura2019PR - stara/zadanie4.py
+++ matura2019PR - stara/zadanie4.py
@@ -36,14 +36,46 @@ def symetria(d1, d2):
     else:
         return False
 
+def kwadrat(dzialka):
+    bok = 0
+    ok = True
+    while ok and bok<30:
+        for i in range(bok+1):
+            if dzialka[i][bok] == "X":
+                ok = False
+                break
+        for i in range(bok+1):
+            if dzialka[bok][i] == "X":
+                ok = False
+                break
+        bok += 1
+    return bok - 1
+
 licznik1 = 0
+zad2 = []
+licznik3 = 0
+zad3 = []
 for dzialka1 in dzialki:
     if trawa(dzialka1):
         licznik1 += 1
 
 for i in range(49):
+    kw = kwadrat(dzialki[i])
+    if kw > licznik3:
+        licznik3 = kw
+for i in range(49):
+    if kwadrat(dzialki[i]) == licznik3:
+        zad3.append(i+1)
+for i in range(49):
     for k in range(49):
         if k != i:
             if symetria(dzialki[i],dzialki[k]):
-                print(k+1, i+1)
-                break
+                if k+1 not in zad2:
+                    zad2.append(k+1)
+                if i+1 not in zad2:
+                    zad2.append(i+1)
+print(licznik1)
+print(zad2)
+print(licznik3)
+print(zad3)
+
